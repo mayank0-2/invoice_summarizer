a524691230a6b19d8c2b7ae2e8903145fd4aec9a
mayank0-2 <mayank02091999@gmail.com>
2026-03-31 22:56:47 +0530

    Intoduct seraching logic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
+from pypdf import PdfReader
+
+
 def main():
-    print("Hello")
+    reader = PdfReader("test.pdf")
+    page = reader.pages[0]
+    text = page.extract_text()
+    index = text.find("Total")
+    temp = text[index:]
+    indexnew = temp.find("\n")
+    print(temp[:indexnew])
+
 
 
 if __name__ == "__main__":
